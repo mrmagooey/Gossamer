84e930a69ac50dfce27b83ac03dafa0ca0047097
Peter Davis <peter.davis8@gmail.com>
2012-02-21 14:37:46 +1100

    Removed urllib2, changed version number

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
-requires = ['lxml', 'urllib2']
+requires = ['lxml']
 
 setup(
     name = 'gossamer',
-    version = '0.1',
+    version = '0.1.1',
     url="https://github.com/mrmagooey/Gossamer",
     author="Peter Davis",
     description="Simple Python Scraper",
