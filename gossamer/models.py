0c3816c35c67333cde7676c8ea5fc470ed3add63
mrmagooey <peter.davis8@gmail.com>
2013-01-20 23:38:21 +1100

    Updates across models and tests, work yet to start on strand models and tests

--- gossamer/models.py
+++ gossamer/models.py
@@ -1,45 +1,104 @@
-
-from tornado.httpclient import AsyncHTTPClient
-from tornado.ioloop import IOLoop
+# -*- coding: utf-8 -*-
+from tornado.httpclient import AsyncHTTPClient, HTTPResponse, HTTPRequest
+#from tornado.ioloop import IOLoop
 from tornado import gen
 from lxml import etree
 import functools
 import os
-
+import re
+from cStringIO import StringIO
 
 class Silk(object):
+    """
+    Core of the gossamer library. Is in charge of:
+    - Parsing start pages (i.e. those given by the `start_urls` variable)
+    - Spawning child strands (based on `strands` variable)
+    - Rate limiting all external fetch requests (based_on )
+    - Returning 
+
+    However, it is not in charge of starting the main event loop and is only passed the
+    pre-made processes event loop singleton, which is .start()ed externally to the class.
+    
+      "The advent of computers, and the subsequent accumulation of incalculable data
+       has given rise to a new system of memory and thought parallel to your own. Humanity
+       has underestimated the consequences of computerization."
+         - Puppet Master
+    """
     current_dir = os.getcwd()
-    local_loop = IOLoop.instance()
+
     
-    
-    def setup(self, loop=local_loop, debug=False, save_directory='debug_html_files'):
-        self.loop = loop.instance()
-        self.client = AsyncHTTPClient(loop)
+    def __init__(self, loop, debug=False, save_directory='debug_html_files',
+                 start_urls=[], allowed_domains=None, fail_silent=True):
+        self.fail_silent = fail_silent
         
+        if allowed_domains:
+            domains = [d.replace('.', r'\.') for d in allowed_domains]
+            self.domain_regex = re.compile(r'(.*\.)?(%s)' % '|'.join(domains))
+        else:
+            self.domain_regex = re.compile(r'')
+            
+        self.loop = loop
+        self.client = AsyncHTTPClient(self.loop)
         self.debug = debug
         if os.path.isabs(save_directory):
             self.save_directory = save_directory
         else:
             self.save_directory = os.path.join(self.current_dir, save_directory)
             
-        if loop is self.local_loop:
-            loop.start()
             
-            
+    def stop(self):
+        """
+        Stops the event loop provided when the Silk() instance was created.
+        
+        Returns True.
+        
+          "Life perpetuates itself through diversity and this includes the ability to
+           sacrifice itself when necessary."
+             - Puppet Master
+        """
+        self.loop.stop()
+        return True
+        
+
     @gen.engine
     def get(self, url, callback):
-        if self.debug:
-            # check if a local copy of url exists
-            local_file = yield gen.Task(self.get_local_file, url)
-            # if so, get the local copy and pass it to callback
-            if local_file:
-                callback(local_file)
-            else:
-                # if not, fetch the url, save a local copy and pass the response to callback
-                callback(self.fetch_and_save_local_file(url))
+        """
+        Takes a target `url` and `callback` function. Asynchronously fetches the `url` and
+        returns a tornado.httpclient.HTTPResponse object to the `callback` function.
 
+        If a allowed_domains is set and a url fails to match the domain name, the function will
+        either return an empty response with response code 418, or raise ExternalDomainError(),
+        depending on whether fail_silent was True in the Silk() instatiation.
+        
+        If debug was set when the Silk object was created, this function will attempt to
+        retrieve the html file from the local disk, and if not found will fetch the remote
+        resource at `url`, save the received response body to disk (at a location
+        determined by the `self.save_directory` variable)and continue to call the
+        `callback` function with the tornado.httpclient.HTTPResonse object. In this way, fast
+        iteration of scraping logic can be implemented using locally saved html objects, rather
+        than continually requesting the same resources from the remote server.
+        
+          "That's all it is. Information."
+            - BatÙ
+
+        """
+        if self.domain_regex.search(url):
+            if self.debug:
+                local_file = yield gen.Task(self.get_local_file, url)
+                if local_file:
+                    callback(local_file)
+                else:
+                    self.fetch_and_save(url, callback)
+            else:
+                self.client.fetch(url, callback)
+        elif not self.fail_silent:
+            raise ExternalDomainError(url)
         else:
-            self.client.fetch(url, callback)
+            fake_request = HTTPRequest(url)
+            buffer = StringIO()
+            buffer.write('')
+            response = HTTPResponse(fake_request, 418, buffer=buffer)
+            callback(response)
             
             
     def _local_file_name(self, url):
@@ -51,18 +110,24 @@ class Silk(object):
         return file_path
 
         
-    def get_local_file(self, url):
-        file_path = self._local_file_name(url)
-        return open(file_path,'rb').read()
-
-    def delete_local_file(self, url):
+    def get_local_file(self, url, callback):
         file_path = self._local_file_name(url)
         try:
-            os.remove(file_path)
-            return True
+            local_file_contents = open(file_path,'rb').read()
+            fake_request = HTTPRequest(url)
+            buffer = StringIO()
+            buffer.write(local_file_contents)
+            response = HTTPResponse(fake_request, 200, buffer=buffer)
+            callback(response)
         except IOError:
-            return False
-
+            callback(None)
+            
+        
+    def delete_local_file(self, url, callback=None):
+        file_path = self._local_file_name(url)
+        os.remove(file_path)
+        if callback:
+            callback()
         
     @gen.engine
     def fetch_and_save(self, url, callback):
@@ -70,13 +135,20 @@ class Silk(object):
         data = yield gen.Task(self.client.fetch, url)
         if not os.path.exists(self.save_directory):
             os.mkdir(self.save_directory)
-        open(file_path,'wb').write(data.body)
+            
+        with open(file_path,'w') as html_file:
+            html_file.write(data.body)
+            html_file.close()
         callback(data)
 
 
     def parse(self, xpath, url, callback):
         """
         Async function that returns a list of matched xpath elements from url to callback.
+
+          "Any way you look at it, all the information that a person accumulates in a
+           lifetime is just a drop in the bucket."
+             - BatÙ
         """
         def _parse(xpath, callback, response):
             html_tree = etree.HTML(response.body)
@@ -84,9 +156,44 @@ class Silk(object):
         p = functools.partial(_parse, xpath ,callback)
         self.get(url, p)
 
+    def run_strands(self):
+        """
+
+        
+          "Each of those things are just a small part of it. I collect information to use in my
+           own way. All of that blends to create a mixture that forms me and gives rise to
+           my conscience."
+            - Major Motoko Kusanagi
+        """
 
 
+class Strand(object):
+    """
+    Calling pattern:
+    
+    Strand(allow, deny, regex, depth, callback)
+    
+    An instance of Silk() can have multiple Strands(), each Strand() may, in turn, have
+    multiple Strands(). A Strand has a single regular expression that it attempts to match
+    to the html that 
+
+    For each strand to be used, it must know:
+       a) who its parent object is (whether Silk() or Strand())
+       b) what regular expressions it should be detecting within a html document
+       c) what the html document is that it should be parsing
+    
+    Each Strand is expecting to be passed some well formatted html from its parent Strand
+    or Silk instance.
+
+        "And where does the newborn go from here? The net is vast and infinite. "
+            - Major Motoko Kusanagi, Puppet Master
+    """    
+    def __init__(self):
+        pass
 
 
-
+class ExternalDomainError(Exception):
+    def __init__(self, url):
+        Exception.__init__(self, "Access outside of allowed_domains attempted: %s"%url)
+    
 
--- gossamer/tests.py
+++ gossamer/tests.py
@@ -1,38 +1,118 @@
-from models import Silk
+from models import Silk, Strand, ExternalDomainError
+import re
 from tornado.testing import AsyncTestCase
+from tornado.httpclient import HTTPResponse
+
 
 class TestSilk(AsyncTestCase):
-    
     def test_can_create_silk_instance(self):
-        s = Silk()
-        s # stop flymake complaining
+        s = Silk(self.io_loop)
+        s.stop()
         
     def test_can_get_page(self):
-        s = Silk()
-        s.setup(self.io_loop)
+        s = Silk(self.io_loop)
         s.get('http://www.google.com', self.stop)
         response = self.wait()
         self.assertIn("Google", response.body)
         
         
     def test_can_parse(self):
-        s = Silk()
-        s.setup(self.io_loop)
+        s = Silk(self.io_loop)
         s.parse('//text()','http://www.google.com', self.stop)
         xpath_elements = self.wait()
         self.assertTrue(type(xpath_elements=='list'))
-
-    
+        
+        
     def test_local_file_storage(self):
-        s = Silk()
-        s.setup(self.io_loop)
+        s = Silk(self.io_loop)
         s.fetch_and_save('http://www.google.com', self.stop)
         response = self.wait()
-        local_file = s.get_local_file('http://www.google.com')
-        self.assertEqual(response.body, local_file)
+        s.get_local_file('http://www.google.com', self.stop)
+        local_file = self.wait()
+        self.assertEqual(response.body, local_file.body)
         s.delete_local_file('http://www.google.com')
         
         
-        
+    def test_debug_setting(self):
+        """
+        Test that with debug=True that files are being saved to the local disk.
+        """
+        s = Silk(self.io_loop, debug=True)
+        s.get('http://www.google.com', self.stop)
+        response = self.wait()
+        s.get_local_file('http://www.google.com', self.stop)
+        cached_response = self.wait()
+        self.assertEqual(response.body, cached_response.body)
+        s.delete_local_file('http://www.google.com')
 
+
+    def test_restrict_scraping_domains(self):
+        domains = [
+            'www.dmoz.org',
+        ]
+        
+        s = Silk(self.io_loop, allowed_domains=domains)
+        s.get('http://www.dmoz.org', self.stop)
+        response = self.wait()
+        self.assertIn("dmoz", response.body)
+
+        s.get('http://google.com', self.stop)
+        response = self.wait()
+        self.assertEqual(response.body, '')
+        
+        domains2 = [
+            'www.google.com',
+        ]
+        
+        s2 = Silk(self.io_loop, allowed_domains=domains2)
+        s2.get('http://www.dmoz.org', self.stop)
+        response = self.wait()
+        self.assertEqual(len(response.body), 0)
+        
+        s2.get('http://google.com', self.stop)
+        response = self.wait()
+        self.assertEqual(len(response.body), 0)
+        
+        domains3 = [
+            'www.dmoz.org',
+            'www.google.com',
+        ]
+        
+        s3 = Silk(self.io_loop, allowed_domains=domains3)
+        s3.get('http://www.dmoz.org', self.stop)
+        response = self.wait()
+        self.assertIn("dmoz", response.body)
+        
+        s3.get('http://www.google.com', self.stop)
+        response = self.wait()
+        self.assertIn("Google", response.body)
+
+        
+    def test_restrict_scraping_domains_fail_loudly(self):
+        domains = [
+            'www.dmoz.org',
+        ]
+        
+        s = Silk(self.io_loop, allowed_domains=domains, fail_silent=False)
+        s.get('http://www.dmoz.org', self.stop)
+        response = self.wait()
+        self.assertIn("dmoz", response.body)
+
+        try:
+            s.get('http://google.com', self.stop)
+        except ExternalDomainError as ex:
+            self.assertEquals(type(ExternalDomainError('')), type(ex))
+        
+        
+class TestStrand(AsyncTestCase):
+    def test_can_create_strand_instance(self):
+        allow_regex = r''
+        st = Strand()
+        st # stop flymake complaining
+        
+    def test_can_attach_strand_to_silk(self):
+        s = Silk(self.io_loop)
+        st = Strand()
+        url = r'google'
+        st
 
