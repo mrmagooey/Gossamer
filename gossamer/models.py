8d1fcef32fce23f8f4c8e259aeed5b2d34effb9e
mrmagooey <peter.davis8@gmail.com>
2013-01-21 13:27:58 +1100

    Strand renamed to Spider. Start of url identification and following methods.

--- gossamer/models.py
+++ gossamer/models.py
@@ -2,7 +2,7 @@
 from tornado.httpclient import AsyncHTTPClient, HTTPResponse, HTTPRequest
 #from tornado.ioloop import IOLoop
 from tornado import gen
-from lxml import etree
+from lxml import html
 import functools
 import os
 import re
@@ -12,13 +12,13 @@ class Silk(object):
     """
     Core of the gossamer library. Is in charge of:
     - Parsing start pages (i.e. those given by the `start_urls` variable)
-    - Spawning child strands (based on `strands` variable)
+    - Spawning spiders (based on `spiders` variable)
     - Rate limiting all external fetch requests (based_on )
-    - Returning 
+    - Returning
 
     However, it is not in charge of starting the main event loop and is only passed the
     pre-made processes event loop singleton, which is .start()ed externally to the class.
-    
+
       "The advent of computers, and the subsequent accumulation of incalculable data
        has given rise to a new system of memory and thought parallel to your own. Humanity
        has underestimated the consequences of computerization."
@@ -26,17 +26,17 @@ class Silk(object):
     """
     current_dir = os.getcwd()
 
-    
+
     def __init__(self, loop, debug=False, save_directory='debug_html_files',
                  start_urls=[], allowed_domains=None, fail_silent=True):
         self.fail_silent = fail_silent
-        
+
         if allowed_domains:
             domains = [d.replace('.', r'\.') for d in allowed_domains]
             self.domain_regex = re.compile(r'(.*\.)?(%s)' % '|'.join(domains))
         else:
             self.domain_regex = re.compile(r'')
-            
+
         self.loop = loop
         self.client = AsyncHTTPClient(self.loop)
         self.debug = debug
@@ -44,21 +44,21 @@ class Silk(object):
             self.save_directory = save_directory
         else:
             self.save_directory = os.path.join(self.current_dir, save_directory)
-            
-            
+
+
     def stop(self):
         """
         Stops the event loop provided when the Silk() instance was created.
-        
+
         Returns True.
-        
+
           "Life perpetuates itself through diversity and this includes the ability to
            sacrifice itself when necessary."
              - Puppet Master
         """
         self.loop.stop()
         return True
-        
+
 
     @gen.engine
     def get(self, url, callback):
@@ -69,7 +69,7 @@ class Silk(object):
         If a allowed_domains is set and a url fails to match the domain name, the function will
         either return an empty response with response code 418, or raise ExternalDomainError(),
         depending on whether fail_silent was True in the Silk() instatiation.
-        
+
         If debug was set when the Silk object was created, this function will attempt to
         retrieve the html file from the local disk, and if not found will fetch the remote
         resource at `url`, save the received response body to disk (at a location
@@ -77,7 +77,7 @@ class Silk(object):
         `callback` function with the tornado.httpclient.HTTPResonse object. In this way, fast
         iteration of scraping logic can be implemented using locally saved html objects, rather
         than continually requesting the same resources from the remote server.
-        
+
           "That's all it is. Information."
             - BatÙ
 
@@ -99,17 +99,35 @@ class Silk(object):
             buffer.write('')
             response = HTTPResponse(fake_request, 418, buffer=buffer)
             callback(response)
-            
-            
+
+    @gen.engine
+    def _crawl(self, url, callback):
+        """
+        Takes urls from Spiders and passes the fetched urls back to the list of Spiders
+
+        """
+
+    @gen.engine
+    def crawl(self, url, callback=None):
+        """
+        get() html from url
+        If callback provided then the silk instance calls the callback on the starting url.
+
+        call parse
+        Pass it to the registered children strands
+        """
+        response = yield gen.Task(self.get, url)
+
+
     def _local_file_name(self, url):
-        # shorten long urls
-        if len(url) > 40:
+
+        if len(url) > 40: # shorten long urls
             url = url[:20] + url[-20:]
         file_name= url.replace('/','_')+ '.html'
         file_path = os.path.join(self.save_directory, file_name)
         return file_path
 
-        
+
     def get_local_file(self, url, callback):
         file_path = self._local_file_name(url)
         try:
@@ -121,79 +139,131 @@ class Silk(object):
             callback(response)
         except IOError:
             callback(None)
-            
-        
+
+
     def delete_local_file(self, url, callback=None):
         file_path = self._local_file_name(url)
         os.remove(file_path)
         if callback:
             callback()
-        
+
+
     @gen.engine
     def fetch_and_save(self, url, callback):
         file_path = self._local_file_name(url)
         data = yield gen.Task(self.client.fetch, url)
         if not os.path.exists(self.save_directory):
             os.mkdir(self.save_directory)
-            
+
         with open(file_path,'w') as html_file:
             html_file.write(data.body)
             html_file.close()
         callback(data)
 
 
-    def parse(self, xpath, url, callback):
+    def parse(self, xpath=None, httpresponse=None, callback=None):
         """
-        Async function that returns a list of matched xpath elements from url to callback.
+        Returns a list of matched `xpath` elements in `httpresponse` to `callback`.
 
           "Any way you look at it, all the information that a person accumulates in a
            lifetime is just a drop in the bucket."
              - BatÙ
         """
-        def _parse(xpath, callback, response):
-            html_tree = etree.HTML(response.body)
-            callback(html_tree.xpath(xpath))
-        p = functools.partial(_parse, xpath ,callback)
-        self.get(url, p)
+        html_tree = html.document_fromstring(httpresponse.body)
+        callback(html_tree.xpath(xpath))
 
-    def run_strands(self):
+
+    def parse_url(self, xpath, url, callback):
         """
+        Async function that returns a list of matched xpath elements from url to callback.
+        """
+        parse_partial = functools.partial(self.parse, xpath, callback=callback)
+        self.get(url, parse_partial)
 
+
+    def register(self, spiders):
+        """
+        Takes either a single spider object, or an iterable of spider objects
         
-          "Each of those things are just a small part of it. I collect information to use in my
-           own way. All of that blends to create a mixture that forms me and gives rise to
-           my conscience."
+          "Each of those things are just a small part of it."
             - Major Motoko Kusanagi
         """
+        try: # Iterable of spiders
+            for spider in spiders:
+                try:
+                    self.spiders.append(spider)
+                except (UnboundLocalError, AttributeError):
+                    self.spiders = [spider]
+        except TypeError: # Single spider
+            try:
+                self.spiders.append(spiders)
+            except (UnboundLocalError, AttributeError):
+                self.spiders = [spiders]
 
 
-class Strand(object):
+class Spider(object):
     """
-    Calling pattern:
+    Spider(allow, deny, depth, callback)
     
-    Strand(allow, deny, regex, depth, callback)
+    An instance of Silk() can have multiple Spiders(), each Spider() may, in turn, have
+    multiple Spiders(). A Spider has a single regular expression that it attempts to match
+    to the html that
     
-    An instance of Silk() can have multiple Strands(), each Strand() may, in turn, have
-    multiple Strands(). A Strand has a single regular expression that it attempts to match
-    to the html that 
-
-    For each strand to be used, it must know:
-       a) who its parent object is (whether Silk() or Strand())
+    For each spider to be used, it must know:
+       a) who its parent object is (whether Silk() or Spider())
        b) what regular expressions it should be detecting within a html document
        c) what the html document is that it should be parsing
     
-    Each Strand is expecting to be passed some well formatted html from its parent Strand
+    Each Spider is expecting to be passed some well formatted html from its parent Spider
     or Silk instance.
+    
+    """
 
+    IGNORED_EXTENSIONS = [
+        # images
+        'mng', 'pct', 'bmp', 'gif', 'jpg', 'jpeg', 'png', 'pst', 'psp', 'tif',
+        'tiff', 'ai', 'drw', 'dxf', 'eps', 'ps', 'svg',
+
+        # audio
+        'mp3', 'wma', 'ogg', 'wav', 'ra', 'aac', 'mid', 'au', 'aiff',
+
+        # video
+        '3gp', 'asf', 'asx', 'avi', 'mov', 'mp4', 'mpg', 'qt', 'rm', 'swf', 'wmv',
+        'm4a',
+
+        # office suites
+        'xls', 'ppt', 'doc', 'docx', 'odt', 'ods', 'odg', 'odp',
+
+        # other
+        'css', 'pdf', 'doc', 'exe', 'bin', 'rss', 'zip', 'rar',
+    ]
+
+    
+    def __init__(self, allow_regex=[], deny_regex=[], depth=None, callback=None):
+        """
         "And where does the newborn go from here? The net is vast and infinite. "
             - Major Motoko Kusanagi, Puppet Master
-    """    
-    def __init__(self):
-        pass
-
+        """
+        self.allow_regex = allow_regex
+        self.deny_regex = deny_regex
+        self.depth = depth
+        self.callback = callback
+        
+        
+    def find_urls(self, httpresponse, callback):
+        """
+        Identifies urls in `httpresponse` and provides links that satisfy `allow_regex`,
+        `deny_regex` and `depth` and returns them as a list to the `callback`.
+        
+        """
+        links=[]
+        for item in html.iterlinks(httpresponse.body):
+            links.append(item)
+        callback(links)
+        
 
 class ExternalDomainError(Exception):
     def __init__(self, url):
         Exception.__init__(self, "Access outside of allowed_domains attempted: %s"%url)
-    
+
 
--- gossamer/tests.py
+++ gossamer/tests.py
@@ -1,27 +1,21 @@
-from models import Silk, Strand, ExternalDomainError
+from models import Silk, Spider, ExternalDomainError
 import re
 from tornado.testing import AsyncTestCase
 from tornado.httpclient import HTTPResponse
-
+from lxml.etree import XPathEvalError
 
 class TestSilk(AsyncTestCase):
     def test_can_create_silk_instance(self):
         s = Silk(self.io_loop)
         s.stop()
+
         
-    def test_can_get_page(self):
+    def test_get(self):
         s = Silk(self.io_loop)
         s.get('http://www.google.com', self.stop)
         response = self.wait()
         self.assertIn("Google", response.body)
-        
-        
-    def test_can_parse(self):
-        s = Silk(self.io_loop)
-        s.parse('//text()','http://www.google.com', self.stop)
-        xpath_elements = self.wait()
-        self.assertTrue(type(xpath_elements=='list'))
-        
+
         
     def test_local_file_storage(self):
         s = Silk(self.io_loop)
@@ -32,6 +26,33 @@ class TestSilk(AsyncTestCase):
         self.assertEqual(response.body, local_file.body)
         s.delete_local_file('http://www.google.com')
         
+        
+    def test_parse_url(self):
+        s = Silk(self.io_loop)
+        s.parse_url('//text()','http://www.google.com', self.stop)
+        xpath_elements = self.wait()
+        self.assertTrue(type(xpath_elements=='list'))
+        
+        
+    def test_parse(self):
+        s = Silk(self.io_loop)
+        s.get('http://www.google.com', self.stop)
+        response = self.wait()
+        s.parse('//text()', response, self.stop)
+        xpath_elements = self.wait()
+        self.assertTrue(type(xpath_elements=='list'))
+        text_string = ''.join(xpath_elements)
+        self.assertIn('google',text_string)
+
+        
+    def test_incorrect_parse_xpath(self):
+        s = Silk(self.io_loop)
+        s.parse_url('//count()','http://www.google.com', self.stop)
+        try:
+            self.wait()
+        except XPathEvalError:
+            pass
+
         
     def test_debug_setting(self):
         """
@@ -50,7 +71,7 @@ class TestSilk(AsyncTestCase):
         domains = [
             'www.dmoz.org',
         ]
-        
+
         s = Silk(self.io_loop, allowed_domains=domains)
         s.get('http://www.dmoz.org', self.stop)
         response = self.wait()
@@ -59,40 +80,40 @@ class TestSilk(AsyncTestCase):
         s.get('http://google.com', self.stop)
         response = self.wait()
         self.assertEqual(response.body, '')
-        
+
         domains2 = [
             'www.google.com',
         ]
-        
+
         s2 = Silk(self.io_loop, allowed_domains=domains2)
         s2.get('http://www.dmoz.org', self.stop)
         response = self.wait()
         self.assertEqual(len(response.body), 0)
-        
+
         s2.get('http://google.com', self.stop)
         response = self.wait()
         self.assertEqual(len(response.body), 0)
-        
+
         domains3 = [
             'www.dmoz.org',
             'www.google.com',
         ]
-        
+
         s3 = Silk(self.io_loop, allowed_domains=domains3)
         s3.get('http://www.dmoz.org', self.stop)
         response = self.wait()
         self.assertIn("dmoz", response.body)
-        
+
         s3.get('http://www.google.com', self.stop)
         response = self.wait()
         self.assertIn("Google", response.body)
 
-        
+
     def test_restrict_scraping_domains_fail_loudly(self):
         domains = [
             'www.dmoz.org',
         ]
-        
+
         s = Silk(self.io_loop, allowed_domains=domains, fail_silent=False)
         s.get('http://www.dmoz.org', self.stop)
         response = self.wait()
@@ -102,17 +123,46 @@ class TestSilk(AsyncTestCase):
             s.get('http://google.com', self.stop)
         except ExternalDomainError as ex:
             self.assertEquals(type(ExternalDomainError('')), type(ex))
-        
-        
-class TestStrand(AsyncTestCase):
-    def test_can_create_strand_instance(self):
-        allow_regex = r''
-        st = Strand()
-        st # stop flymake complaining
-        
-    def test_can_attach_strand_to_silk(self):
-        s = Silk(self.io_loop)
-        st = Strand()
-        url = r'google'
-        st
 
+            
+
+class TestSpider(AsyncTestCase):
+
+    def test_can_create_spider_instance(self):
+        Spider()
+        
+        allow_regex = [r'Python',r'Ruby']
+        deny_regex = [r'Deutsch']
+        depth = 1
+        Spider(allow_regex, deny_regex, depth=depth, callback=None)
+        
+
+    def test_can_register_spiders(self):
+        spider1 = Spider()
+        spider2 = Spider()
+        spider3 = Spider()
+        
+        s = Silk(self.io_loop)
+        s.register(spider1)
+        s.register([spider2,spider3])
+
+        self.assertIn(spider1, s.spiders)
+        self.assertIn(spider2, s.spiders)
+        self.assertIn(spider3, s.spiders)
+        
+    def test_spider_prints_urls_without_callback(self):
+        allow_regex = ['Python','Ruby']
+        deny_regex = ['Deutsch']
+        
+        spider1 = Spider(allow_regex, deny_regex, depth=None, callback=None)
+        s = Silk(self.io_loop, allowed_domains=['www.dmoz.org'], fail_silent=False)
+        s.register(spider1)
+        s.crawl('http://www.dmoz.org/Computers/Programming/Languages/Python/Books/',
+                self.stop)
+        
+        
+        
+        
+        
+        
+        
